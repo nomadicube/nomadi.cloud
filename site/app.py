dbaf64165e46239ab45bf9278453ba798796cc42
0xfrogz <136229101+0xfrogz@users.noreply.github.com>
2024-11-28 10:42:09 +0100

    init

--- site/app.py
+++ site/app.py
@@ -13,5 +13,5 @@ def pgp():
     return render_template('pgp.html') 
 
 if __name__ == '__main__': 
-    app.run(debug=True, host='0.0.0.0', port=80)
-
+    #app.run(debug=True, host='0.0.0.0', port=80)
+    app.run()
